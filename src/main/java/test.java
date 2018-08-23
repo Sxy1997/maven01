f55507dd88e9203ea3fdf455ea637b32b63d5dd6
Sxy1997 <2264012891>
2018-08-23 20:03:05 +0800

    添加了一个实体类

---
+++ src/main/java/test.java
@@ -0,0 +1,23 @@
+/**
+ * Created by ls on 2018/8/23.
+ */
+public class test {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
